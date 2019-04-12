2136bd44d76c93d107cca62a2258ed99df45d245
Antti Eskola <antti.eskola@gmail.com>
2019-04-12 09:47:43 +0300

    missing client

---
+++ Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var one = new One.BookServiceOneClient())
+            using (var two = new Two.BookServiceTwoClient())
+            {
+                var rOne = one.GetAll();
+                var rTwo = two.GetAll();
+                var both = rOne.Concat(rTwo);
+            }
+        }
+    }
+}
